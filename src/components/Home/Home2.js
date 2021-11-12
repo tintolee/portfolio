cb7ec3bea29af24085ed70df0e8e1f71a4a42f3d
tinto <toso4u@gmail.com>
2021-11-12 15:49:30 +0100

    nn

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,16 +24,16 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> Javascript and Python. </b>
+                <b className="purple"> Javascript(reactJS, expressJS, ) and Python(django). </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My field of Interests are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">web and mobile Technologies/Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
+                  Health care solution products
                 </b>
               </i>
               <br />
