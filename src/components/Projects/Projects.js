4c971b467f3dc89659ee07285fa8a68732fa6eba
tintolee <toso4u@gmail.com>
2022-03-16 14:50:37 +0100

    updated project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -22,17 +22,16 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          {/* <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
               title="Chatify"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              link="https://github.com/soumyajit4419/Chatify"
+              link="https://chatify-49.web.app/"
             />
-          </Col> */}
+          </Col>
 
-{/* hhjhjh */}
 
           <Col md={4} className="project-card">
             <ProjectCard
